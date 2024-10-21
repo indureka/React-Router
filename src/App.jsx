37cd8a7b8538d81d41dbeaacd79506bcf206946d
indureka <165481162+indureka@users.noreply.github.com>
2024-10-21 19:09:55 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import ProductList from './components/productList'
+import ProductList from './components/ProductList'
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import Cart from './components/Cart';
 import { CartProvider } from './context/CartContext';
